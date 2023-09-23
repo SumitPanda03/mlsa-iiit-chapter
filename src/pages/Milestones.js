3ca37cf9fec947a049a59dca774a3243dc7992b8
Sumit Panda <sumitpanda1308@gmail.com>
2023-09-23 23:09:10 +0530

    Added animation to Milestones

--- src/pages/Milestones.js
+++ src/pages/Milestones.js
@@ -1,211 +1,219 @@
 import React from "react";
 import "../CSS_files/Milestones.css";
 import "animate.css";
-import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { VscAzure, VscGithub,VscProject, VscTwitter } from "react-icons/vsc";
+import { FaLinkedin, FaCode, FaSquareGit, FaMicrosoft,FaBrain, FaShirt,FaSocks, FaBottleWater, FaCube, FaLaptop } from "react-icons/fa6";
+import { MdVideoCameraFront } from "react-icons/md";
+import { IoShirtSharp, IoRibbonSharp } from "react-icons/io5";
+import { PiCertificateBold } from "react-icons/pi";
 
 function Milestones() {
-  return (
-    <>
-      <Navbar />
-      <div className="container">
-        <div className="parent-container">
-          <div className="container1">
-            <div className="child1">
-              <div className="new-image-container">
-                <img
-                  className="new-image"
-                  src="./assets/LevelNew.png"
-                  alt="MLSA New Logo"
-                />
-              </div>
+	return (
+		<>
+			<Navbar />
+			<div className="container">
+				<div className="timeline">
+					<div className="timeline-row">
+						<div className="timeline-time" >
+							 NEW MLSA
+						</div>
 
-              <div className="alpha-container">
-                <h3>NEW MLSA</h3>
-                <div className="alpha-child">
-                  <p>
-                    Student Ambassadors are required to complete various
-                    pre-identified activities in order to grow their skills on a
-                    well-defined road to success. As you complete these
-                    activities, your program milestones are advanced. Additional
-                    program benefits are unlocked too. Milestones are advanced
-                    in order from{" "}
-                    <strong style={{ color: "#4b6cb7" }}>New to Alpha</strong>,{" "}
-                    <strong style={{ color: "#4b6cb7" }}>Alpha to Beta</strong>{" "}
-                    and{" "}
-                    <strong style={{ color: "#4b6cb7" }}>Beta to Gold</strong>.
-                    Program certificates are updated with each milestone
-                    advancement. Milestones showcase your expertise and let the
-                    world know about your achievements. You are encouraged to
-                    share your milestone advancements with your{" "}
-                    <strong style={{ color: "#4b6cb7" }}>local</strong> and{" "}
-                    <strong style={{ color: "#4b6cb7" }}>online</strong>
-                    &nbsp;communities.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <hr />
-          <div className="container2">
-            <div className="child2">
-              <div className="alpha-image-container">
-                <img
-                  className="alpha-image"
-                  src="./assets/LevelAlpha.png"
-                  alt="MLSA Alpha Logo"
-                />
-              </div>
+						<div className="timeline-content">
+							<img
+								className="img-fluid rounded"
+								src="./assets/LevelNew.png"
+								alt=" NEW MLSA"
+							/>
+							<p>
+								Student Ambassadors are required to complete various
+								pre-identified activities in order to grow their skills on a
+								well-defined road to success. As you complete these activities,
+								your program milestones are advanced. Additional program
+								benefits are unlocked too. Milestones are advanced in order from{" "}
+								<strong style={{ color: "#e4d08f" }}>New to Alpha</strong>,{" "}
+								<strong style={{ color: "#e4d08f" }}>Alpha to Beta</strong> and{" "}
+								<strong style={{ color: "#e4d08f" }}>Beta to Gold</strong>.
+								Program certificates are updated with each milestone
+								advancement. Milestones showcase your expertise and let the
+								world know about your achievements. You are encouraged to share
+								your milestone advancements with your{" "}
+								<strong style={{ color: "#e4d08f" }}>local</strong> and{" "}
+								<strong style={{ color: "#e4d08f" }}>online</strong>&nbsp;
+								communities.
+							</p>
+							<div className="thumbs">
+								<div className="thumbs">
+									<img
+										className="img-fluid rounded"
+										src="./assets/avatar1.png"
+										alt="Maxwell Admin"
+									/>
+									<img
+										className="img-fluid rounded"
+										src="./assets//avatar2.png"
+										alt="Maxwell Admin"
+									/>
+									<img
+										className="img-fluid rounded"
+										src="./assets/avatar3.png"
+										alt="Maxwell Admin"
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
 
-              <div className="alpha-container">
-                <h3>ALPHA MLSA</h3>
-                <div className="alpha-child">
-                  <p>
-                    Student Ambassadors that complete a learning path on
-                    Microsoft Learn will earn the{" "}
-                    <strong style={{ color: "#4b6cb7" }}>Alpha</strong> badge.
-                  </p>
-                </div>
-                <div className="alpha-perks">
-                  <ul>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        150$ monthly Azure credits
-                      </strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        Visual Studio Enterprise subscription
-                      </strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        StudentAmbassadors.com email address
-                      </strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        Event workshop kits
-                      </strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        Incentives for event attendees
-                      </strong>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+					<div className="timeline-row">
+						<div className="timeline-time">
+							 ALPHA MLSA
+						</div>
+						<div className="timeline-content">
+							<img
+								className="img-fluid rounded"
+								src="./assets/LevelAlpha.png"
+								alt=" ALPHA MLSA"
+							/>
+							<p>
+								Student Ambassadors that complete a learning path on Microsoft
+								Learn will earn the Alpha badge.
+							</p>
+							<ul>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>
+										150$ monthly Azure credits
+									</strong>
+								</li>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>
+										Visual Studio Enterprise subscription
+									</strong>
+								</li>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>
+										StudentAmbassadors.com email address
+									</strong>
+								</li>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>
+										Event workshop kits
+									</strong>
+								</li>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>
+										Incentives for event attendees
+									</strong>
+								</li>
+							</ul>
+							<div className="thumbs">
+								<div className="thumbs">
+									<VscAzure/>
+									<FaLinkedin/>
+									<FaCode/>
+								</div>
+							</div>
+						</div>
+					</div>
 
-          <hr />
+					<div className="timeline-row">
+						<div className="timeline-time">
+							 BETA MLSA
+						</div>
+						<div className="timeline-content">
+							<img
+								className="img-fluid rounded"
+								src="./assets/LevelBeta.png"
+								alt=" BETA MLSA"
+							/>
+							<p>
+								Student Ambassadors who plan and host an event for other
+								students in their community will earn the Beta badge.
+							</p>
+							<ul>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>Alpha benefits</strong>
+								</li>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>Swag</strong>
+								</li>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>Event support</strong>
+								</li>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>Mentorship</strong>
+								</li>
+							</ul>
+							<div className="thumbs">
+								<div className="thumbs">
+									<FaShirt/>
+									<FaBottleWater/>
+									<FaSocks/>
+                  <FaCube/>
+								</div>
+							</div>
+						</div>
+					</div>
 
-          <div className="container3">
-            <div className="child3">
-              <div className="beta-image-container">
-                <img
-                  className="beta-image"
-                  src="./assets/LevelBeta.png"
-                  alt="MLSA Beta Logo"
-                />
-              </div>
-              <div className="beta-container">
-                <h3>BETA MLSA</h3>
-                <div className="beta-child">
-                  <p>
-                    Student Ambassadors who plan and host an event for other
-                    students in their community will earn the{" "}
-                    <strong style={{ color: "#4b6cb7" }}>Beta</strong> badge.
-                  </p>
-                </div>
-                <div className="beta-perks">
-                  <ul>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        Alpha benefits
-                      </strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>Swag</strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        StudentAmbassadors.com email address
-                      </strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        Event support
-                      </strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>Mentorship</strong>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+					<div className="timeline-row">
+						<div className="timeline-time">
+							GOLD MLSA
+						</div>
+						<div className="timeline-content">
+							<img
+								className="img-fluid rounded"
+								src="./assets/LevelGold.png"
+								alt=" GOLD MLSA"
+							/>
+							<p>
+								Student Ambassadors who have gone above and beyond serving
+								their local community will be invited to the Gold milestone.
+							</p>
+							<ul>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>
+										Alpha and Beta benefits
+									</strong>
+								</li>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>
+										Program leadership opportunities
+									</strong>
+								</li>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>
+										Microsoft Most Valuable Professional (MVP) mentorship
+									</strong>
+								</li>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>
+										May be invited to the MVP program
+									</strong>
+								</li>
+								<li>
+									<strong style={{ color: "#e4d08f" }}>
+										Exclusive Gold swag
+									</strong>
+								</li>
+							</ul>
+							<div className="thumbs">
+								<div className="thumbs">
+									<FaLaptop/>
+									<FaMicrosoft/>  
+									<MdVideoCameraFront/>
+                  <IoShirtSharp/>
+                  <IoRibbonSharp/>
+                  <PiCertificateBold/>
 
-          <hr />
-
-          <div className="container4">
-            <div className="child4">
-              <div className="gold-image-container">
-                <img
-                  className="gold-image"
-                  src="./assets/LevelGold.png"
-                  alt="MLSA Gold Logo"
-                />
-              </div>
-              <div className="gold-container">
-                <h3>GOLD MLSA</h3>
-                <div className="gold-child">
-                  <p>
-                    Student Ambassadors who have gone above and beyond serving
-                    their local community will be invited to the{" "}
-                    <strong style={{ color: "#4b6cb7" }}>Gold</strong>{" "}
-                    milestone.
-                  </p>
-                </div>
-                <div className="gold-perks">
-                  <ul>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        Alpha and Beta benefits
-                      </strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        Program leadership opportunities
-                      </strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        Microsoft Most Valuable Professional (MVP) mentorship
-                      </strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        May be invited to the MVP program
-                      </strong>
-                    </li>
-                    <li>
-                      <strong style={{ color: "#4b6cb7" }}>
-                        Exclusive Gold swag
-                      </strong>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default Milestones;
